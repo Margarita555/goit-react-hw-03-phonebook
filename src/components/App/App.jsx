a59c3d5160bb9797af9132752953e47119a12c90
Margarita555 <a_rita@ukr.net>
2021-12-03 19:20:55 +0200

    +

---
+++ src/components/App/App.jsx
@@ -0,0 +1,78 @@
+import { Component } from 'react/cjs/react.production.min';
+import { nanoid } from 'nanoid';
+import ContactForm from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
+import Filter from '../Filter/Filter';
+import s from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleAddContact = ({ name, number }) => {
+    const isDuplicateContact = this.state.contacts.find(
+      contact => contact.name === name,
+    );
+    if (isDuplicateContact) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  onFilterChange = e => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  getVisibleContacts = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase()),
+    );
+  };
+
+  handleDeleteContact = name => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.name !== name),
+    }));
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <div className={s.app}>
+        <h1>Phonebook</h1>
+        <ContactForm
+          contacts={this.state.contacts}
+          onSubmit={this.handleAddContact}
+        />
+        <h2>Contacts</h2>
+        <Filter
+          filter={this.state.filter}
+          onFilterChange={this.onFilterChange}
+        />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteBtn={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
